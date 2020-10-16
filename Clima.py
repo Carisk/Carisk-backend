d1009e3f5dfa2832e4ae7b6571ecdfc1fc192115
juninhigh <54211866+juninhigh@users.noreply.github.com>
2020-10-16 14:19:12 -0300

    Update Clima.py
    Aumentando a precisão das coordenadas.

--- Clima.py
+++ Clima.py
@@ -1,5 +1,12 @@
 import requests
-from pprint import pprint #so para imprimir o jason de forma mais legível
+from pprint import pprint #so para imprimir o json de forma mais legível
+
+#Para aumentar a precisão das coordenadas
+res = requests.get('https://ipinfo.io/')
+data = res.json()
+location = data['loc'].split(',')
+latitude = location[0]
+longitude = location[1]
 
 #Coordenadas desejadas
 lat = input("Digite a latitude desejada: ")
@@ -9,7 +16,7 @@ linguagem = 'pt'
 #api
 url = 'http://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&lang={}&appid=0700d5b772b06c3fc81ae8c2c0773c9c'.format(lat,lon, linguagem)
 res = requests.get(url)
-print(res) #testando se as coordenadas para ver se retornam algum erro
+#print(res) #testando se as coordenadas para ver se retornam algum erro
 
 #dados que API retorna
 data = res.json()
